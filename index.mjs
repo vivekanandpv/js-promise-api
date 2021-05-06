48e085c769b88c79edf0e6b63c99f4cac66200aa
vivek <vivek@athenaeum.in>
2021-05-06 08:34:57 +0530

    done

--- index.mjs
+++ index.mjs
@@ -14,9 +14,18 @@ function cube(n) {
   });
 }
 
+function increment(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(n + 1);
+    }, 500);
+  });
+}
+
 square(5)
   .then((result) => cube(result)) //  cube returns promise
-  .then((result) => console.log('then', result)) //  chained; result comes from cube now
+  .then((result) => increment(result)) //  increment returns promise
+  .then((result) => console.log('then', result)) //  chained; result comes from increment now
   .catch((error) => console.log('catch', error))
   .finally(() => console.log('finally'));
 
@@ -24,6 +33,6 @@ console.log('End of script');
 
 //  because promise is asynchronous, we see:
 //  End of script
-//  -- 2 seconds delay
-//  then 15625
+//  -- 2.5 seconds delay
+//  then 15626      ((5^2)^3 + 1 = 15626)
 //  finally
